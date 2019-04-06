a5933967853024fcbda4c90fe009edb5d0e21005
2012jithu <jithu2012@outlook.com>
2019-04-06 04:50:21 +0200

    Add files via upload
    initial commit

---
+++ src/com/sample/readdata/EmailUtil.java
@@ -0,0 +1,89 @@
+package com.sample.readdata;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final String fromEmail = "dagli.khushbu@gmail.com"; //requires valid gmail id
+		final String password = "kerela2406"; // correct password for gmail id
+		final String toEmail = "jithu2012@gmail.com"; // can be any email id 
+		
+		System.out.println("SSLEmail Start");
+		Properties props = new Properties();
+		/*props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+		props.put("mail.smtp.socketFactory.port", "587"); //SSL Port
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+		props.put("mail.smtp.auth", "true"); //enable authentication
+		props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+		props.put("mail.smtp.port", "587"); //TLS Port
+*/		
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "587");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "587");
+		Authenticator auth = new Authenticator() {
+			//override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+		
+		Session session = Session.getDefaultInstance(props, auth);
+		System.out.println("Session created");
+	        EmailUtil.sendEmail(session, toEmail,"SSLEmail Testing Subject", "SSLEmail Testing Body");
+		
+	}
+	
+	/**
+	 * Utility method to send simple HTML email
+	 * @param session
+	 * @param toEmail
+	 * @param subject
+	 * @param body
+	 */
+	public static void sendEmail(Session session, String toEmail, String subject, String body){
+		try
+	    {
+	      MimeMessage msg = new MimeMessage(session);
+	      //set message headers
+	      msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+	      msg.addHeader("format", "flowed");
+	      msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+	      msg.setFrom(new InternetAddress("dagli.khushb@gmail.com", "NoReply-JD"));
+
+	      msg.setReplyTo(InternetAddress.parse("jithu2012@gmail.com", false));
+
+	      msg.setSubject(subject, "UTF-8");
+
+	      msg.setText(body, "UTF-8");
+
+	      msg.setSentDate(new Date());
+
+	      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+	      System.out.println("Message is ready");
+    	  Transport.send(msg);  
+
+	      System.out.println("EMail Sent Successfully!!");
+	    }
+	    catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	}
+
+}
